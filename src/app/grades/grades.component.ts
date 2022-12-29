096c3394ce9e7d7d7c1a651a19acd5fe4db40669
createview <58891993+kubabin@users.noreply.github.com>
2022-12-29 11:35:06 +0100

    sorting grades by subject works

--- src/app/grades/grades.component.ts
+++ src/app/grades/grades.component.ts
@@ -23,19 +23,46 @@ export class GradesComponent implements OnInit {
   ngOnInit(): void {
 
   }
+  private easterEggValue=0;
+  easterEgg()
+  {
+    if (this.easterEggValue==0)
+    {
+      this.easterEggValue=1;
+    }
+    else if (this.easterEggValue==1)
+    {
+      this.easterEggValue=2;
+    }
+    else if (this.easterEggValue==2)
+    {
+      this.easterEggValue=3;
+    }
+    else if (this.easterEggValue==3)
+    {
+      let grades:HTMLCollectionOf<Element> = document.getElementsByClassName("gradeBadge")
+      for (let i:number = 0; i != grades.length; i++)
+      {
+
+      }
+    }
+  }
   shuffled()
   {
     return this.grades.sort(() => Math.random() - 0.5);
   }
   sortBySubject(grades:Array<Grade>, subject:Subject):Array<Grade> {
     let sortedArray:Array<Grade> = [];
+    let i:number=0;
     for (let grade of grades)
     {
-      if (grade.subject == subject)
+      if (grade.subject.displayName == subject.displayName)
       {
         sortedArray.push(grade)
       }
+      i++
     }
+
     return sortedArray;
   }
   sortByValue(grades:Array<Grade>, value:string)
--- src/app/homework.ts
+++ src/app/homework.ts
@@ -1,18 +1,27 @@
+import { Subject } from "./subject";
+
 export class Homework {
 
-  constructor(id:number = Math.floor(Math.random() * (Math.floor(10000) - Math.floor(1) + 1)) + Math.floor(1),content:string, createdBy:string, createdDate:Date, completeBy:Date)
+  constructor(id:number = Math.floor(Math.random() * (Math.floor(10000) - Math.floor(1) + 1)) + Math.floor(1),
+  content:string,
+  createdBy:string,
+  createdDate:Date,
+  completeBy:Date,
+  subject:Subject)
   {
     this.content=content;
     this.createdBy=createdBy;
     this.createdDate=createdDate;
     this.completeBy=completeBy;
     this.id=id;
+    this.subject=subject;
   }
   id:number
   content:string;
   createdBy:string;
   createdDate:Date;
   completeBy:Date;
+  subject:Subject;
   getJson(){
     return {"content":this.content, "createdBy":this.createdBy, "createdDate":this.createdDate, "completeBy": this.completeBy}
   }
